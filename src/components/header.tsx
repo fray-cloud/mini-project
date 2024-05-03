0477bdb6f9252813282c80a56c2e91ebf14f8429
wh <inco123@naver.com>
2024-05-04 07:57:32 +0900

    Header update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,15 +2,19 @@ import { Nav, Navbar, Container } from "react-bootstrap";
 
 const Header = () => {
     return(
-        <Navbar className="navBar">
+        <Navbar collapseOnSelect className="navBar" bg="dark" data-bs-theme="dark">
             <Container>
                 <Navbar.Brand href="/">Home</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="navbarScroll">
                 <Nav 
                     className="me-auto"
                     defaultActiveKey="/"
                 >
                     <Nav.Link href="/main">Main</Nav.Link>
+                    <Nav.Link href="#">Main2</Nav.Link>
                 </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     )
