566b0fbd66929ec3703709b6dc89142bfca70769
wh <inco123@naver.com>
2024-05-04 07:57:57 +0900

    Components update

--- src/site/index.tsx
+++ src/site/index.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+// import { sidoGET, Sido, Sigungu, sigunguGET } from "../api/callAPI";
+import { Badge, Stack, Container } from "react-bootstrap";
+import SidoComponent from "./find/sido";
+import SigunguComponent from "./find/sigungu";
+import KindComponent from "./find/kind";
+
 
 const Main = () => {
+
     return(
-        <div>Hi</div>
+        <>
+            <SidoComponent>
+                <SigunguComponent/>
+            </SidoComponent>
+            <KindComponent/>
+        </>
     )
 }
 
