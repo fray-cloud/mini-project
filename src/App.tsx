c298e0e0b0d516107df6bfb0fc875c2dc62c653f
wh <inco123@naver.com>
2024-05-14 22:56:30 +0900

    업데이트

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import SiteRouter from './router/intex';
 import Header from './components/header';
 import { Container } from 'react-bootstrap';
